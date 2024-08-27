ac66118a22984b28e4f738e648093ec05fe25b61
JegathaM21 <146633863+JegathaM21@users.noreply.github.com>
2024-08-27 20:54:12 +0530

    Add files via upload

---
+++ IntershipProjects/calculator/script.js
@@ -0,0 +1,21 @@
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function deleteLast() {
+    const display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
+
+function appendCharacter(character) {
+    document.getElementById('display').value += character;
+}
+
+function calculate() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = 'Error';
+    }
+}
